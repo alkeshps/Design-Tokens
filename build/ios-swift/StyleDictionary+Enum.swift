c90b547613da460bd012585c608ae5a28cfb8465
Alkesh Padmakumar <alkeshpadmakumar@gmail.com>
2024-11-07 12:08:13 +0530

    radius changed

--- build/ios-swift/StyleDictionary+Enum.swift
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 public enum StyleDictionaryEnum {
-    public static let lightThemeInfBrandColor = UIColor(red: 0.910, green: 0.118, blue: 0.118, alpha: 1)
+    public static let lightThemeInfBrandColor = UIColor(red: 0.145, green: 0.118, blue: 0.808, alpha: 0.93)
     public static let lightThemeInfPrimaryBgColor = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
     public static let lightThemeInfPrimaryTextColor = UIColor(red: 0.122, green: 0.122, blue: 0.122, alpha: 1)
     public static let lightThemeInfRoundedLg = 16px
-    public static let lightThemeInfRoundedMd = 8px
+    public static let lightThemeInfRoundedMd = 100px
     public static let lightThemeInfRoundedSm = 4px
     public static let lightThemeInfRoundedXl = 24px
     public static let lightThemeInfSecondaryBgColor = UIColor(red: 0.953, green: 0.961, blue: 0.973, alpha: 1)
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let lightThemeInfBrandColor = UIColor(red: 0.910, green: 0.118, blue: 0.118, alpha: 1)
+    internal static let lightThemeInfBrandColor = UIColor(red: 0.145, green: 0.118, blue: 0.808, alpha: 0.93)
     internal static let lightThemeInfPrimaryBgColor = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
     internal static let lightThemeInfPrimaryTextColor = UIColor(red: 0.122, green: 0.122, blue: 0.122, alpha: 1)
     internal static let lightThemeInfRoundedLg = 16px
-    internal static let lightThemeInfRoundedMd = 8px
+    internal static let lightThemeInfRoundedMd = 100px
     internal static let lightThemeInfRoundedSm = 4px
     internal static let lightThemeInfRoundedXl = 24px
     internal static let lightThemeInfSecondaryBgColor = UIColor(red: 0.953, green: 0.961, blue: 0.973, alpha: 1)
